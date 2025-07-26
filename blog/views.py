be7151f7bdf6ebaff262d56e19be866e3c349ec3
SPraveeen <praveenji8498@gmail.com>
2025-07-26 10:09:23 +0530

    first_view, url's, dynamic urls, redirects, reverse and named urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Hi')
+
+def detail(request,post_id):
+    return HttpResponse(f"You are viewing post detail page and ID is {post_id}")
+
+def old_url_redirect(request):
+    return redirect(reverse('blog:new_page_url'))
+
+def new_url_view(request):
+    return HttpResponse("This is the new URL")
