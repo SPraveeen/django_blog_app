9bb2201eb79f291bbeb85a1662a8bb91b28bd375
SPraveeen <praveenji8498@gmail.com>
2025-07-28 21:23:29 +0530

    Many to One Relationship//Deleting and Recreating Tables// Show Category in Template// Showing Related Posts -> error needs to fix

--- blog/management/commands/populate_posts.py
+++ blog/management/commands/populate_posts.py
@@ -1,5 +1,6 @@
-from blog.models import Post
+from blog.models import Post,Category
 from django.core.management.base import BaseCommand
+import random
 
 class Command(BaseCommand):
     help="This command inserts post data"
@@ -75,8 +76,10 @@ class Command(BaseCommand):
             "https://picsum.photos/id/20/800/400",
         ]
 
+        categories=Category.objects.all()
         for title,content,img_url in zip(titles,contents,img_urls):
-            Post.objects.create(title=title,content=content,img_url=img_url)
+            category=random.choice(categories)
+            Post.objects.create(title=title,content=content,img_url=img_url,category=category)
 
         self.stdout.write(self.style.SUCCESS("Completed Insertingg Data!"))
         
