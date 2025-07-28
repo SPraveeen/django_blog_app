28966902f97934aa06c71e5693f71af197ee716d
SPraveeen <praveenji8498@gmail.com>
2025-07-28 06:41:50 +0530

    Get Post by Slug - Creating Category Model for Post

---
+++ blog/management/commands/populate_categories.py
@@ -0,0 +1,17 @@
+from blog.models import Category
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help="This command inserts Categroy data"
+
+    def handle(self,*args: any,**options: any):
+        # delete existing data
+        Category.objects.all().delete()
+
+        categories=['Sports','Technology','Science','Art','Food']
+
+        for category_name in categories:
+            Category.objects.create(name=category_name)
+
+        self.stdout.write(self.style.SUCCESS("Completed Inserting Data!"))
+        
