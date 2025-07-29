69b9ac536214094cbabbcc99f2c8b360e0273272
SPraveeen <praveenji8498@gmail.com>
2025-07-29 08:46:54 +0530

    Showing Related Posts-Pagination-Working with Contact Form{Error not showing contact}

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 import logging
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -17,8 +19,14 @@ from django.http import Http404
 def index(request):
     blog_title="Latest Posts"
     # getting data from post model
-    posts=Post.objects.all()
-    return render(request,'index.html',{'blog_title':blog_title,'posts':posts})
+    all_posts=Post.objects.all()
+
+    # paginate
+    paginator=Paginator(all_posts,5)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+
+    return render(request,'index.html',{'blog_title':blog_title,'page_obj':page_obj})
 
 def detail(request,slug):
 
@@ -35,10 +43,18 @@ def detail(request,slug):
 
     # logger=logging.getLogger("TESTING")
     # logger.debug(f'post variable is {post}')
-    return render(request,'detail.html',{'related_posts':related_posts})
+    return render(request,'detail.html',{'post':post,'related_posts':related_posts})
 
 def old_url_redirect(request):
     return redirect(reverse('blog:new_page_url'))
 
 def new_url_view(request):
     return HttpResponse("This is the new URL")
+
+def contact_view(request):
+    if request.method=='POST':
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            logger=logging.getLogger("TESTING")
+            logger.debug(f'post data is {form.cleaned_data['name']} {form.cleaned_data['email']} {form.cleaned_data['message']}')
+    return render(request,'contact.html')
